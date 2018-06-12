de5744126d7b5255e59d82ab06d81c8c0b579498
Joona Janhunen <joona.janhunen@stud.pointcollege.fi>
2018-06-12 20:14:08 +0300

    KouluMuutoksia

--- Assets/Koodit/Movement/RuutuLiikkuminen3.cs
+++ Assets/Koodit/Movement/RuutuLiikkuminen3.cs
@@ -31,10 +31,10 @@ public class RuutuLiikkuminen3 : MonoBehaviour
 
     public static RuutuLiikkuminen3 Instance;
 
-    string viimepositiox = @"C:\Tiedontallennusharjoitus\Viimepositiotiedostox.txt";
-    string viimepositioy = @"C:\Tiedontallennusharjoitus\Viimepositiotiedostoy.txt";
-    string viimepositioz = @"C:\Tiedontallennusharjoitus\Viimepositiotiedostoz.txt";
-    string nykyinenpositio = @"C:\Tiedontallennusharjoitus\Nykyinenpositiotiedosto.txt";
+    //string viimepositiox = @"C:\Tiedontallennusharjoitus\Viimepositiotiedostox.txt";
+    //string viimepositioy = @"C:\Tiedontallennusharjoitus\Viimepositiotiedostoy.txt";
+    //string viimepositioz = @"C:\Tiedontallennusharjoitus\Viimepositiotiedostoz.txt";
+    //string nykyinenpositio = @"C:\Tiedontallennusharjoitus\Nykyinenpositiotiedosto.txt";
 
 
     void Awake()
@@ -56,9 +56,9 @@ public class RuutuLiikkuminen3 : MonoBehaviour
         liikkunut = false;
         pos = transform.position;          
  
-        File.WriteAllText(viimepositiox, pos.x.ToString());
-        File.WriteAllText(viimepositioy, pos.y.ToString());
-        File.WriteAllText(viimepositioz, pos.z.ToString());
+        //File.WriteAllText(viimepositiox, pos.x.ToString());
+        //File.WriteAllText(viimepositioy, pos.y.ToString());
+        //File.WriteAllText(viimepositioz, pos.z.ToString());
 
     }
 
@@ -143,9 +143,9 @@ public class RuutuLiikkuminen3 : MonoBehaviour
             if (Input.GetKey(KeyCode.A) && transform.position == pos)
             {        // Left
                 RakettiÄäni.Play();
-                File.WriteAllText(viimepositiox, pos.x.ToString());
-                File.WriteAllText(viimepositioy, pos.y.ToString());
-                File.WriteAllText(viimepositioz, pos.z.ToString());
+                //File.WriteAllText(viimepositiox, pos.x.ToString());
+                //File.WriteAllText(viimepositioy, pos.y.ToString());
+                //File.WriteAllText(viimepositioz, pos.z.ToString());
                 pos += Vector3.left;
                 PelaajaViimeliike = "vasen";
 
@@ -157,9 +157,9 @@ public class RuutuLiikkuminen3 : MonoBehaviour
             if (Input.GetKey(KeyCode.D) && transform.position == pos)
             {        // Right
                 RakettiÄäni.Play();
-                File.WriteAllText(viimepositiox, pos.x.ToString());
-                File.WriteAllText(viimepositioy, pos.y.ToString());
-                File.WriteAllText(viimepositioz, pos.z.ToString());
+                //File.WriteAllText(viimepositiox, pos.x.ToString());
+                //File.WriteAllText(viimepositioy, pos.y.ToString());
+                //File.WriteAllText(viimepositioz, pos.z.ToString());
                 pos += Vector3.right;
                 PelaajaViimeliike = "oikea";
 
@@ -171,9 +171,9 @@ public class RuutuLiikkuminen3 : MonoBehaviour
             if (Input.GetKey(KeyCode.W) && transform.position == pos)
             {        // Up
                 RakettiÄäni.Play();
-                File.WriteAllText(viimepositiox, pos.x.ToString());
-                File.WriteAllText(viimepositioy, pos.y.ToString());
-                File.WriteAllText(viimepositioz, pos.z.ToString());
+                //File.WriteAllText(viimepositiox, pos.x.ToString());
+                //File.WriteAllText(viimepositioy, pos.y.ToString());
+                //File.WriteAllText(viimepositioz, pos.z.ToString());
                 pos += Vector3.up;
                 PelaajaViimeliike = "ylös";
 
@@ -185,9 +185,9 @@ public class RuutuLiikkuminen3 : MonoBehaviour
             if (Input.GetKey(KeyCode.S) && transform.position == pos)
             {        // Down
                 RakettiÄäni.Play();
-                File.WriteAllText(viimepositiox, pos.x.ToString());
-                File.WriteAllText(viimepositioy, pos.y.ToString());
-                File.WriteAllText(viimepositioz, pos.z.ToString());
+                //File.WriteAllText(viimepositiox, pos.x.ToString());
+                //File.WriteAllText(viimepositioy, pos.y.ToString());
+                //File.WriteAllText(viimepositioz, pos.z.ToString());
                 pos += Vector3.down;
                 PelaajaViimeliike = "alas";
 
@@ -202,14 +202,14 @@ public class RuutuLiikkuminen3 : MonoBehaviour
             transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
 
 
-        if (transform.position != pos)
-        {
+        //if (transform.position != pos)
+        //{
             
-        }
-        if (transform.position==pos)
-        {
-            File.WriteAllText(nykyinenpositio, pos.ToString());
-        }
+        //}
+        //if (transform.position==pos)
+        //{
+        //    File.WriteAllText(nykyinenpositio, pos.ToString());
+        //}
         
 
      
